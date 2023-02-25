439a0c6dbf9c756abfce4394d90c6e11d3692f94
mhmmt <muhokcud@gmail.com>
2023-02-25 11:40:16 +0100

    Test2 Updated, Test5 Uploaded

--- src/Test/Test2.java
+++ src/Test/Test2.java
@@ -1,4 +1,8 @@
 package Test;
 
 public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("Test sinif 2");
+        System.out.println("Test degisiklik");
+    }
 }
